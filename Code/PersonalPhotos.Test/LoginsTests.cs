dd058f3de8c89996c43da88448a96ef7c057a489
prakaski <kiran.prakash@sunquestinfo.com>
2021-10-06 15:57:27 +0530

    Added LoginTests Class

--- Code/PersonalPhotos.Test/LoginsTests.cs
+++ Code/PersonalPhotos.Test/LoginsTests.cs
@@ -1,14 +1,30 @@
+using PersonalPhotos.Controllers;
 using System;
 using Xunit;
+using Moq;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PersonalPhotos.Test
 {
     public class LoginsTests
     {
-        [Fact]
-        public void Test1()
+        private readonly LoginsController _controller;
+        private readonly Mock<ILogins> _logins;
+        private readonly Mock<IHttpContextAccessor> _accessor;
+        public LoginsTests()
         {
-
+            _logins = new Mock<ILogins>();
+            _accessor = new Mock<IHttpContextAccessor>();
+            _controller = new LoginsController(_logins.Object, _accessor.Object);
+        }
+        [Fact]
+        public void GivenNorReturnUrl_ReturnLoginView()
+        {
+            var result = (_controller.Index() as ViewResult);
+            Assert.NotNull(result);
+            Assert.Equal("Login", result.ViewName, ignoreCase: true);
         }
     }
 }
